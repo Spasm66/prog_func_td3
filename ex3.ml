cef0394b2752904b7983d1f5c79a05cdc37fc31a
Loris PFEIFFER <loris.pfeiffer@u-bordeaux.fr>
2026-09-22 16:26:47 +0200

    first

---
+++ ex3.ml
@@ -0,0 +1,42 @@
+(*ex 3.1*)
+let combine (x, y) f = f x y
+
+let util x y p = if p x y then combine (x,y) (fun u v -> (v,u)) else x,y
+
+(*ex 3.3*)
+let x = 2, true, "ok"
+let y = (3, false), "sam"
+let z = 12, (true, "mas")
+
+(*ex 3.4*)
+type carburant = Diesel | Essence | Electrique
+type vehicule = {
+    carburant c;
+    int nb_roues;
+}
+let clio = vehicule(Diesel, 4)
+let make_vehicule c n = vehicule(c, n)
+let carburant_of v = v.c
+let nb_weels_of v = v.nb_roues
+let can_run v = if carburant_of v = Diesel && nb_weels_of v >= 4
+    then false else true
+let consommation v n = let p_km = match carburant_of v with
+    | Diesel -> (6. *. 1.5) /. 100.
+    | Essence -> (8. *. 1.5) /. 100.
+    | Electrique -> (10. *. 0.25) /. 100.
+    in p_km *. n
+
+(*ex 3.5*)
+type date = {
+    day: int;
+    month: int;
+    year: int;
+}
+let today = {day = 22; month = 09; year = 2026}
+let date_infeg date1 date2 = if date1.year > date2.year 
+    then false
+    else if date1.month > date2.month 
+        then false
+    else date1.day < date2.day
+
+(*ex 3.6*)
